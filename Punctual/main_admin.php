3950fb2d8a9271d67d7e7a21a8b25abcba7a5c1d
sid-tripathi <46405836+sid-tripathi@users.noreply.github.com>
2021-04-03 17:44:00 -0400

    Main page for admin view

---
+++ Punctual/main_admin.php
@@ -0,0 +1,51 @@
+<?php
+// Start session
+session_start();
+ 
+// Checks if user is logged in, if they aren't then redirect
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Main Page</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<style>
+			.wrapper {
+				margin: auto;
+				width: 500px;
+				padding: 25px;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="wrapper">
+			<h1>Punctual</h1>
+			<br>
+			<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+				<div>
+					<label>Join a Room</label>
+				</div>
+				<div>
+					<a href="join.php" class="btn btn-primary">Join!</a>
+				</div>
+				<br>
+				<div>
+					<label>Create a Room</label>
+				</div>
+				<div>
+					<a href="create.php" class="btn btn-primary">Create!</a>
+				</div>			
+				<br>
+				<br>
+				<div>
+					<a href="logout.php" class="btn btn-danger">Log Out</a>
+				</div>
+			</form>
+		</div>
+	</body>
+</html>
